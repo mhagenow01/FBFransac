a3cfd95ad8f9da14faedb387938933ba7bc1f1b7
Kevin Welsh <kevinwelsh132@gmail.com>
2020-02-19 21:19:58 -0600

    Unbroke that test thing

--- test_screw_scene.py
+++ test_screw_scene.py
@@ -5,6 +5,7 @@ from scipy.spatial.transform import Rotation
 from mpl_toolkits.mplot3d import Axes3D
 from Mesh import Mesh
 from ModelFinder import ModelFinder
+import point_cloud_utils as pcu
 import Verbosifier
 
 if __name__ == '__main__':
@@ -21,14 +22,17 @@ if __name__ == '__main__':
     mesh = Mesh('Models/ToyScrew-Yellow.stl')
     mesh.compileFeatures(N = 10)
     finder = ModelFinder(mesh)
+    mask = ModelFinder.voxelFilter(fullCloud, size = 0.005)
+    cloudNormals = pcu.estimate_normals(fullCloud, 5)
+    cloud, cloudNormals = fullCloud[mask], cloudNormals[mask]
 
 
     fig = plt.figure()
     ax = fig.gca(projection = '3d')
 
-    ax.scatter(fullCloud[:,0], fullCloud[:,1], fullCloud[:,2])
+    ax.scatter(cloud[:,0], cloud[:,1], cloud[:,2])
     count = 0
-    for o, r in finder.findInCloud(fullCloud):
+    for o, r in finder.findInCloud(cloud, cloudNormals):
         print(o, r)
         faces = (mesh.Faces @ r.T) + o
         ax.scatter(faces[:,0], faces[:,1], faces[:,2], c = 'r')
