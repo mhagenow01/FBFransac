43f83cb535863898465475257bf07522b10539b1
Kevin Welsh <kevinwelsh132@gmail.com>
2020-02-23 20:24:45 -0600

    Added SegmentTree_test and tried it with more realistic features.
    Can be very slow, depending on the particular set of features.

--- SegmentTree.py
+++ SegmentTree.py
@@ -1,10 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt 
-from ncls import NCLS
 import itertools
-import time
-from cProfile import Profile
-import pstats
+from Verbosifier import verbose
 
 class Interval:
     __slots__ = ['Start', 'End']
@@ -12,15 +8,6 @@ class Interval:
         self.Start = start
         self.End = end
 
-    @classmethod
-    def fromList(cls, l):
-        d = len(l)
-        interval = Interval(np.zeros((1,d)), np.zeros((1,d)))
-        for i in range(d):
-            interval.Start[0,i] = l[i].Start[0,0]
-            interval.End[0,i] = l[i].End[0,0]
-        return interval
-    
     def __and__(self, other):
         ''' True if the open intervals have an intersection. False otherwise '''
         return not (np.any(self.Start >= other.End) or np.any(other.Start >= self.End))
@@ -29,7 +16,7 @@ class Interval:
         ''' Returns the union of two intervals '''
         return Interval(np.min((self.Start, other.Start), axis = 0), np.max((self.End, other.End), axis = 0))
 
-    def __mod__(self, other):
+    def __mul__(self, other):
         ''' Returns an interval representing the intersection of both intervals
             Assumes that self & other == True.
         '''
@@ -37,10 +24,10 @@ class Interval:
 
     def __xor__(self, other):
         ''' Returns a tuple of distinct intervals such that the union of the intervals 
-            along with self % other results in the union of self and other.
+            along with self * other results in the union of self and other.
         '''
         ret = []
-        intersection = self % other
+        intersection = self * other
         union = self | other
         d = self.Start.shape[1]
         for combo in itertools.product((-1,0,1), repeat = d):
@@ -70,16 +57,11 @@ class Interval:
     def __le__(self, other):
         return np.all(self.Start >= other.Start) and np.all(self.End <= other.End)
 
-
     def contains(self, p):
         return not (np.any(p < self.Start) or np.any(p > self.End))
     
     def __repr__(self):
         return f'[{self.Start},{self.End}]'
-    
-    def __mul__(self, other):
-        return Interval(np.concatenate((self.Start, other.Start), axis = 1), 
-                        np.concatenate((self.End, other.End), axis = 1))
 
 class Node:
     def __init__(self, intervals, dimension = 1, splitDimenion = 0):
@@ -106,7 +88,6 @@ class Node:
             if self.Left is not None and self.Right is not None:
                 self.Interval = self.Left.Interval | self.Right.Interval
         return
-
 
     def insertInterval(self, interval):
         self.Count += 1
@@ -193,12 +174,24 @@ class Node:
 class SegmentTree:
     def __init__(self, intervals):
 
-        elementaryIntervals = self.buildElementaryIntervalsFast(intervals)
-        self.Root = Node(elementaryIntervals, intervals.shape[2])
+        elementaryIntervals = SegmentTree.buildElementaryIntervalsFast(intervals)
+        self.buildElementaryTree(elementaryIntervals, intervals.shape[2])
+        self.insertRealIntervals(intervals)
+        self.pruneLeaves()
+        return
+
+    @verbose()
+    def buildElementaryTree(self, elementaryIntervals, dimension):
+        self.Root = Node(elementaryIntervals, dimension)
+
+    @verbose()
+    def insertRealIntervals(self, intervals):
         for i in intervals:
             self.Root.insertInterval(Interval(i[0], i[1]))
+    
+    @verbose()
+    def pruneLeaves(self):
         self.Root.prune()
-        return
 
     @staticmethod
     def getPositionInList(elementaryIntervals, interval):
@@ -212,18 +205,21 @@ class SegmentTree:
             if elementaryIntervals[mid].Start[0,0] < interval.Start[0,0]:
                 low = mid
             else:
+
                 high = mid
         if elementaryIntervals[low].Start[0,0] < interval.Start[0,0]:
             low += 1
         return low
 
-    def buildElementaryIntervalsFast(self, intervals):
+    @staticmethod
+    @verbose()
+    def buildElementaryIntervalsFast(intervals):
         intervals = [Interval(i[0:1,:], i[1:2,:]) for i in intervals]
         intervals.sort(key = lambda x: x.Start[0,0])
         elementaryIntervals = [intervals.pop(0)]
         while len(intervals) > 0:
             toAdd = intervals.pop(0)
-            i = self.getPositionInList(elementaryIntervals, toAdd)
+            i = SegmentTree.getPositionInList(elementaryIntervals, toAdd)
             i = max(0,i)
             while i < len(elementaryIntervals):
                 e = elementaryIntervals[i]
@@ -231,7 +227,7 @@ class SegmentTree:
                     break
                 if toAdd & e:
                     elementaryIntervals.remove(e)
-                    intersection = toAdd % e
+                    intersection = toAdd * e
                     j = i
                     while j < len(elementaryIntervals) and elementaryIntervals[j].Start[0,0] > intersection.Start[0,0]:
                         j += 1
@@ -243,7 +239,6 @@ class SegmentTree:
             else:
                 elementaryIntervals.insert(i,toAdd)
         return elementaryIntervals
-                
 
     def containsPoint(self, p):
         return self.Root.containsPoint(p)
@@ -254,54 +249,6 @@ class SegmentTree:
     def renderTo2d(self, ax):
         self.Root.renderTo2d(ax)
 
-
-def imshowTree(tree):
-    plt.figure()
-    x = np.arange(-4, 4, 0.01)
-    y = np.arange(-4, 4, 0.01)
-    X, Y = np.meshgrid(x, y)
-    Z = np.zeros_like(X)
-    for i, _x in enumerate(x):
-        for j, _y in enumerate(y):
-            Z[i,j] = 1 if tree.containsPoint(np.array((_x, _y))) else 0
-    plt.imshow(Z.T, origin = 1, extent=[-4,4,-4,4])
-
-
-
 if __name__ == '__main__':
-    d = 10
-    n = 100000
-    e = 0.001
-    error = np.array((e) * d)
-    intervals = [
-        [n - error, n + error] for n in np.random.randn(n,d)
-    ]
-    # intervals = [
-    #     [[0,0], [1,1]],
-    #     [[0.5, 0.5], [2,2]]
-    # ]
-    # dx = 0.05
-    # intervals = [
-    #     [[x,x], [x+2*dx, x+2*dx]] for x in np.arange(-2, 2, dx)
-    # ]
-    intervals = np.array(intervals)
-    s = time.time()
-    tree = SegmentTree(intervals)
-    print(f'Compile time with {n}-{d} dimensional boxes of size {e}: {time.time() - s}')
-
-    fig = plt.figure()
-    ax = fig.gca()
-    ax.set_xlim([-4,4])
-    ax.set_ylim([-4,4])
-    s = time.time()
-    for q in np.random.randn(10000,d):
-        tree.containsPoint(q)
-    print(f'Query time 10k points: {time.time() - s}')
-    #tree.renderTo2d(ax)
-    # imshowTree(tree)
-    # x = np.mean(intervals[:,:,0], axis = 1)
-    # y = np.mean(intervals[:,:,1], axis = 1)
-    #plt.scatter(x, y)
-    #plt.show()  
-    
+    pass
 
---
+++ SegmentTree_test.py
@@ -0,0 +1,100 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+from cProfile import Profile
+import pstats
+import functools
+import itertools
+import time
+from Mesh import Mesh
+from SegmentTree import SegmentTree
+import Verbosifier
+
+
+
+###### Test Methods ######
+
+def timedRandomSet():
+    d = 10
+    n = 100000
+    e = 0.001
+    error = np.array((e) * d)
+    intervals = [
+        [n - error, n + error] for n in np.random.randn(n,d)
+    ]
+    intervals = np.array(intervals)
+    s = time.time()
+    tree = SegmentTree(intervals)
+    print(f'Compile time with {n}-{d} dimensional boxes of size {e}: {time.time() - s}')
+    for q in np.random.randn(10000,d):
+        tree.containsPoint(q)
+    print(f'Query time 10k points: {time.time() - s}')
+
+
+def imshowTree(tree):
+    plt.figure()
+    x = np.arange(-4, 4, 0.01)
+    y = np.arange(-4, 4, 0.01)
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros_like(X)
+    for i, _x in enumerate(x):
+        for j, _y in enumerate(y):
+            Z[i,j] = 1 if tree.containsPoint(np.array((_x, _y))) else 0
+    plt.imshow(Z.T, origin = 1, extent=[-4,4,-4,4])
+
+def showSimple():
+    dx = 0.05
+    intervals = [
+        [[x,x], [x+2*dx, x+2*dx]] for x in np.arange(-2, 2, dx)
+    ]
+    intervals = np.array(intervals)
+    tree = SegmentTree(intervals)
+
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.set_xlim([-4,4])
+    ax.set_ylim([-4,4])
+    tree.renderTo2d()
+    imshowTree(tree)
+
+
+def angleAndDistance(n1, n2, vertices, verts):
+    v1, v2 = vertices[verts[0]], vertices[verts[1]]
+    r = v2 - v1
+    d = np.linalg.norm(r)
+    r /= d
+    return np.arccos(abs(n1.dot(r))), np.arccos(abs(n2.dot(r))), d
+
+
+def testAngleDistanceFeature():
+    mesh = Mesh('Models/ToyScrew-Yellow.stl')
+    nFaces = len(mesh.Faces)
+
+    intervals = []
+    for i in range(nFaces):
+        for j in range(i + 1, nFaces):
+            f1, f2 = mesh.Faces[[i,j]]
+            n1, n2 = mesh.Normals[[i,j]]
+            r = f2 - f1
+
+            v_ind1, v_ind2 = mesh.trimesh.faces[[i,j]]
+            distances = np.array(list( map(functools.partial(angleAndDistance, n1, n2, mesh.trimesh.vertices), itertools.product(v_ind1, v_ind2)) ))
+            minDistace = np.min(distances, axis = 0)
+            maxDistance = np.max(distances, axis = 0)
+            if np.any(np.isnan(minDistace)) or np.any(np.isnan(maxDistance)):
+                continue
+            intervals.append([minDistace, maxDistance])
+    intervals = np.array(intervals)
+    intervals = intervals[np.random.choice(range(len(intervals)), 400)]
+    s = time.time()
+    tree = SegmentTree(intervals)
+    print(f'Feature compile time: {time.time() - s}')
+    s = time.time()
+    for q in (np.random.rand(10000,3) * np.array((np.pi/2, np.pi/2, 0.05))):
+        tree.containsPoint(q)
+    print(f'Query time 10k points: {time.time() - s}')
+
+    
+if __name__ == '__main__':
+    Verbosifier.enableVerbosity()
+    testAngleDistanceFeature()
+
