f43b8ded1bc60ec9cf3cd47b29ac547e9ff402c2
Kevin Welsh <kevinwelsh132@gmail.com>
2020-04-08 15:02:33 -0500

    more robust sphere finding

--- test_sphere_thing.py
+++ test_sphere_thing.py
@@ -8,11 +8,9 @@ from pykdtree.kdtree import KDTree
 from scipy.spatial import cKDTree
 
 class SupportSphere:
-    MaxShift = 0.001
-    MinShift = 0.00005
-    dr = 0.00001
-    Epsilon = 0.00000001
-    MaxIterations = 1000
+    dr = 0.01
+    Epsilon = 0.0001
+    MaxIterations = 100
     def __init__(self, x):
         self.X = x
         self.R = 0
@@ -21,36 +19,65 @@ class SupportSphere:
     def update(self, cloud, kd : cKDTree):
         self.Iterations += 1
         if self.Iterations > self.MaxIterations:
+            print('ahh')
             return False
 
-        distance, index = kd.query(self.X, 4, distance_upper_bound = self.R + self.dr)
+        distance, index = kd.query(self.X, 400, eps=0, distance_upper_bound = self.R + self.dr)
         distance, index = distance[0], index[0]
         index = index[~np.isinf(distance)]
         distance = distance[~np.isinf(distance)]
-        if len(distance) > 3:
+        touchingDistance = distance[distance <= self.R + self.Epsilon]
+        touchingIndex = index[distance <= self.R + self.Epsilon]
+        if len(touchingDistance) >= 3:
             return True
         if len(distance) == 0:
             self.R += self.dr
             return None
-        if len(distance) == 1:
-            p = cloud[index[0]]
+
+        if len(touchingDistance) == 0:
+            dr = distance[0] - self.R
+            self.R += dr
+        if len(touchingDistance) == 1:
+            p = cloud[touchingIndex[0]]
+            dr = self.dr
             dx = self.X - p
-            dx *= self.dr / np.linalg.norm(dx)
-            self.X += dx
-            self.R += self.dr
-        if len(distance) == 2:
-            p1, p2 = cloud[index]
+            dx /= np.linalg.norm(dx)
+            dr = self.tryMove(dx, dr, cloud, index[1:])
+        if len(touchingDistance) == 2:
+            p1, p2 = cloud[touchingIndex]
             midPoint = (p1 + p2) / 2
+            dr = self.dr
             dx = self.X - midPoint
-            dx *= np.sqrt((self.R + self.dr) ** 2 - self.R ** 2) / np.linalg.norm(dx)
-            self.X += dx
-            self.R += self.dr
-        if len(distance) == 3:
-            p1, p2, p3 = cloud[index]
+            dx /= np.linalg.norm(dx)
+            a = np.linalg.norm(p1 - midPoint)
+            b = np.sqrt((self.R + self.dr)**2 - a**2)
+            dr = self.tryMove(dx, b - np.sqrt(self.R**2 - a**2), cloud, index[2:])
+        if len(touchingDistance) == 3:
+            p1, p2, p3 = cloud[touchingIndex]
             n = np.cross(p2 - p1, p3 - p1)
-            self.X += self.dr * n / np.linalg.norm(n) / 4
+            dr = self.dr / 4
+            self.X += dr * n / np.linalg.norm(n)
+        if dr < self.Epsilon:
+            return True
         return None
 
+    def tryMove(self, dx, d, cloud, nearByPointsIndex):
+        nearbyPoints = cloud[nearByPointsIndex]
+        distanceAlongAxis = np.sum(dx * (nearbyPoints - self.X), axis = 1)
+        distanceToAxis = np.linalg.norm(nearbyPoints - self.X - np.outer(distanceAlongAxis, dx), axis = 1)
+
+        distanceAlongAxis = distanceAlongAxis[distanceToAxis <= self.R]
+        distanceToAxis = distanceToAxis[distanceToAxis <= self.R]
+        
+        distanceToCollision = (np.abs(distanceAlongAxis) - np.sqrt(self.R**2 - distanceToAxis**2)) \
+                                * np.sign(distanceAlongAxis)
+        willCollide = (distanceToCollision > 0) & (distanceToCollision < 2 * d)
+        prevDistance = np.linalg.norm(nearbyPoints - self.X, axis = 1)
+
+        if np.any(willCollide):
+            d = np.min(distanceToCollision[willCollide]) / 2
+        self.X += dx * d
+        return d
         
 def drawSphere(ax, X, R):
     u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
@@ -63,7 +90,7 @@ r = 0.008
 spheres = [SupportSphere(np.array([(0.0,0.0,0.0)]))]
 def update(ax, cloudArtist, cloud, kd, t):
     global spheres
-    for i in range(50):
+    for i in range(1):
         s = spheres[-1]
         converged = s.update(cloud, kd)
         if converged is not None or s.R > r:
@@ -71,15 +98,9 @@ def update(ax, cloudArtist, cloud, kd, t):
             topRight = np.max(cloud, axis = 0).reshape((1,3))
             nextStart = np.random.random(3) * (topRight - bottomLeft) + bottomLeft
             if converged:
-                spheres.append(
-                    SupportSphere(
-                        nextStart
-                    )
-                )
+                spheres.append(SupportSphere(nextStart))
             else:
-                spheres[-1] = SupportSphere(
-                                nextStart
-                            )
+                spheres[-1] = SupportSphere(nextStart)
     ax.clear()
     cloudArtist = ax.scatter(cloud[:,0], cloud[:,1], cloud[:,2])
     for s in spheres:
