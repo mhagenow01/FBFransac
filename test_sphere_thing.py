ab2ac8c1cd667c35e228bbcab1434e631b42847e
Kevin Welsh <kevinwelsh132@gmail.com>
2020-04-28 10:10:52 -0500

    some sphere thing

--- test_sphere_thing.py
+++ test_sphere_thing.py
@@ -7,115 +7,101 @@ from matplotlib.animation import FuncAnimation
 from pykdtree.kdtree import KDTree
 from scipy.spatial import cKDTree
 
-class SupportSphere:
-    dr = 0.01
-    Epsilon = 0.0001
-    MaxIterations = 100
-    def __init__(self, x):
+EPSILON = 0.00001
+
+class BetterSupportSphere:
+    def __init__(self, x, maxIter = 100):
         self.X = x
-        self.R = 0
-        self.Iterations = 1
+        self.Iterations = 0
+        self.MaxIter = maxIter
     
-    def update(self, cloud, kd : cKDTree):
+    def update(self, cloud, kd, radius, dr):
+
         self.Iterations += 1
-        if self.Iterations > self.MaxIterations:
-            print('ahh')
+        if self.Iterations >= self.MaxIter:
             return False
 
-        distance, index = kd.query(self.X, 400, eps=0, distance_upper_bound = self.R + self.dr)
+        distance, index = kd.query(self.X, 100, distance_upper_bound = 1.1*radius)
         distance, index = distance[0], index[0]
         index = index[~np.isinf(distance)]
+        
         distance = distance[~np.isinf(distance)]
-        touchingDistance = distance[distance <= self.R + self.Epsilon]
-        touchingIndex = index[distance <= self.R + self.Epsilon]
-        if len(touchingDistance) >= 3:
+        if len(distance) < 20:
+            return False
+        internalPoints = distance < (radius - dr/2)
+        if ~np.any(internalPoints) and sum(distance <= radius + dr/2) > 20:
             return True
-        if len(distance) == 0:
-            self.R += self.dr
-            return None
-
-        if len(touchingDistance) == 0:
-            dr = distance[0] - self.R
-            self.R += dr
-        if len(touchingDistance) == 1:
-            p = cloud[touchingIndex[0]]
-            dr = self.dr
-            dx = self.X - p
-            dx /= np.linalg.norm(dx)
-            dr = self.tryMove(dx, dr, cloud, index[1:])
-        if len(touchingDistance) == 2:
-            p1, p2 = cloud[touchingIndex]
-            midPoint = (p1 + p2) / 2
-            dr = self.dr
-            dx = self.X - midPoint
-            dx /= np.linalg.norm(dx)
-            a = np.linalg.norm(p1 - midPoint)
-            b = np.sqrt((self.R + self.dr)**2 - a**2)
-            dr = self.tryMove(dx, b - np.sqrt(self.R**2 - a**2), cloud, index[2:])
-        if len(touchingDistance) == 3:
-            p1, p2, p3 = cloud[touchingIndex]
-            n = np.cross(p2 - p1, p3 - p1)
-            dr = self.dr / 4
-            self.X += dr * n / np.linalg.norm(n)
-        if dr < self.Epsilon:
-            return True
+        
+        points = cloud[index]
+        dx = self.X - points
+        dx = ((dx.T) / np.linalg.norm(dx, axis = 1)).T
+        
+        weights = 1 / (1 + np.exp((distance - radius) / (dr/2)))
+        #weights = np.exp(-(distance)**2 / (2 * radius**2))
+        dx = np.sum((((radius - distance) * dx.T) * weights) / np.sum(weights), axis = 1).T
+        self.X += dx
+        if np.linalg.norm(dx) < EPSILON:
+            return False
         return None
-
-    def tryMove(self, dx, d, cloud, nearByPointsIndex):
-        nearbyPoints = cloud[nearByPointsIndex]
-        distanceAlongAxis = np.sum(dx * (nearbyPoints - self.X), axis = 1)
-        distanceToAxis = np.linalg.norm(nearbyPoints - self.X - np.outer(distanceAlongAxis, dx), axis = 1)
-
-        distanceAlongAxis = distanceAlongAxis[distanceToAxis <= self.R]
-        distanceToAxis = distanceToAxis[distanceToAxis <= self.R]
-        
-        distanceToCollision = (np.abs(distanceAlongAxis) - np.sqrt(self.R**2 - distanceToAxis**2)) \
-                                * np.sign(distanceAlongAxis)
-        willCollide = (distanceToCollision > 0) & (distanceToCollision < 2 * d)
-        prevDistance = np.linalg.norm(nearbyPoints - self.X, axis = 1)
-
-        if np.any(willCollide):
-            d = np.min(distanceToCollision[willCollide]) / 2
-        self.X += dx * d
-        return d
-        
+   
 def drawSphere(ax, X, R):
     u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
     x = X[0,0] + R * np.cos(u)*np.sin(v)
     y = X[0,1] + R * np.sin(u)*np.sin(v)
+
     z = X[0,2] + R * np.cos(v)
     ax.plot_surface(x, y, z, color="r")
 
-r = 0.008
-spheres = [SupportSphere(np.array([(0.0,0.0,0.0)]))]
-def update(ax, cloudArtist, cloud, kd, t):
+r = 0.0064
+dr = 0.001
+spheres = [BetterSupportSphere(np.array([[0.,0.,0.]]))]
+def update(ax, cloud, kd, t):
     global spheres
-    for i in range(1):
-        s = spheres[-1]
-        converged = s.update(cloud, kd)
-        if converged is not None or s.R > r:
-            bottomLeft = np.min(cloud, axis = 0).reshape((1,3))
-            topRight = np.max(cloud, axis = 0).reshape((1,3))
-            nextStart = np.random.random(3) * (topRight - bottomLeft) + bottomLeft
-            if converged:
-                spheres.append(SupportSphere(nextStart))
-            else:
-                spheres[-1] = SupportSphere(nextStart)
+    pos = None
+    while pos is None:
+        pos = spheres[-1].update(cloud, kd, r, dr)
+    bottomLeft = np.min(cloud, axis = 0).reshape((1,3))
+    topRight = np.max(cloud, axis = 0).reshape((1,3))
+    nextPos = np.random.random(3) * (topRight - bottomLeft) + bottomLeft
+    if isinstance(pos, bool):
+        spheres[-1] = BetterSupportSphere(np.array(nextPos))
+    else:
+        spheres.append(BetterSupportSphere(np.array(nextPos)))
     ax.clear()
-    cloudArtist = ax.scatter(cloud[:,0], cloud[:,1], cloud[:,2])
+    ax.scatter(cloud[:,0], cloud[:,1], cloud[:,2])
     for s in spheres:
-        drawSphere(ax, s.X, s.R)
+        drawSphere(ax, s.X, r)
     return None
+
+
+def profileObject(cloud, minR, maxR, stepSize):
+    kd = cKDTree(cloud)
+    keyPoints = {}
+    bottomLeft = np.min(cloud, axis = 0).reshape((1,3))
+    topRight = np.max(cloud, axis = 0).reshape((1,3))
+    for r in np.arange(minR, maxR+0.00000001, stepSize):
+        spheres = []
+        for i in range(1000):
+            startPos = np.random.random(3) * (topRight - bottomLeft) + bottomLeft
+            s = BetterSupportSphere(startPos)
+            found = None
+            while found is None:
+                found = s.update(cloud, kd, r, 0.1 * r)
+            if found:
+                spheres.append(s)
+        keyPoints.append((r, spheres))
+    return keyPoints
+
+
+
 
 if __name__ == '__main__':
     with open('Models/Cloud_ToyScrew-Yellow.json') as fin:
         cloud = np.array(json.load(fin))
+        #cloud = cloud[cloud[:,0] > 0]
     
-    ax = plt.gca(projection = '3d')
-    cloudArtist = ax.scatter(cloud[:,0], cloud[:,1], cloud[:,2])
-
-    ani = FuncAnimation(plt.gcf(), functools.partial(update, ax, cloudArtist, cloud, cKDTree(cloud)), [1])
-
-    plt.show()
+    keyPoints = profileObject(cloud, 0.003, 0.008, 0.0002)
+    for k,v in keyPoints.items():
+        print(k, len(v))
 
     
